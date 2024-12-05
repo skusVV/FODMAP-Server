f6cc37b821ea9abd828440b925583570b8657003
BorivojTodorcic <borivoj.todorcic@gmail.com>
2024-12-05 10:27:30 +0000

    test 5

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  mode: 'production', // Add this line
+  resolve: {
+    extensions: ['.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist'),
+    libraryTarget: 'commonjs2' // Add this line to handle module exports correctly
+  },
+  target: 'node',
+};
